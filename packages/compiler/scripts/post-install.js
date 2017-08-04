6583d1e7c86861d848fabbf57559efc2afdd4fcc
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2017-08-05 00:08:55 +0300

    test

--- packages/compiler/scripts/post-install.js
+++ packages/compiler/scripts/post-install.js
@@ -5,13 +5,13 @@ console.log("Setting up symlinks...");
 
 fs.symlinkSync(path.join(__dirname, "..", "configs", ".babelrc"), "./../../../.babelrc");
 fs.symlinkSync(path.join(__dirname, "..", "configs", ".editorconfig"), "./../../../.editorconfig");
-fs.symlinkSync(path.join(__dirname, "..", "configs", "tsconfig.json"), "./../../../tsconfig.son");
-fs.symlinkSync(path.join(__dirname, "..", "configs", "tslint.json"), "./../../../tslint.son");
-fs.symlinkSync(path.join(__dirname, "..", "configs", "webpack.js"), "./../../../webpack.s");
+fs.symlinkSync(path.join(__dirname, "..", "configs", "tsconfig.json"), "./../../../tsconfig.json");
+fs.symlinkSync(path.join(__dirname, "..", "configs", "tslint.json"), "./../../../tslint.json");
+fs.symlinkSync(path.join(__dirname, "..", "configs", "jest.config.js"), "./../../../jest.config.js");
 
 if (fs.existsSync("./../../../__tests__")) {
     fs.symlinkSync(path.join(__dirname, "..", "configs", "tsconfig.tests.json"), "./../../../__tests__/tsconfig.json");
-    fs.symlinkSync(path.join(__dirname, "..", "configs", "jest-beforeTest.js"), "./../../../__tests__/beforeTest.js");
+    fs.symlinkSync(path.join(__dirname, "..", "configs", "jest-beforeTest.js"), "./../../../__tests__/jest-beforeTest.js");
 }
 
 console.log("Done !");
