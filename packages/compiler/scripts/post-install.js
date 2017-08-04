009ca2ae1f03eab9263499bdd33183d7ad65764d
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2017-08-04 23:50:49 +0300

    fix

--- packages/compiler/scripts/post-install.js
+++ packages/compiler/scripts/post-install.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+console.log('Setting up symlinks...')
+
 fs.symlinkSync(path.join(__dirname, "..", "configs", ".babelrc"), path.resolve("./.babelrc"));
 fs.symlinkSync(path.join(__dirname, "..", "configs", ".editorconfig"), path.resolve("./.editorconfig"));
 fs.symlinkSync(path.join(__dirname, "..", "configs", "tsconfig.json"), path.resolve("./tsconfig.json"));
@@ -11,3 +13,5 @@ if (fs.existsSync("./__tests__")) {
     fs.symlinkSync(path.join(__dirname, "..", "configs", "tsconfig.tests.json"), path.resolve("./__tests__/tsconfig.json"));
     fs.symlinkSync(path.join(__dirname, "..", "configs", "jest-beforeTest.js"), path.resolve("./__tests__/beforeTest.js"));
 }
+
+console.log('Done !');
