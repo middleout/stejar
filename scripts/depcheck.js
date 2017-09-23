60abae665ef4debfe0eb1f5e36c0013d7a5b966d
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2017-09-23 12:46:08 +0300

    wip

--- scripts/depcheck.js
+++ scripts/depcheck.js
@@ -1,40 +1,54 @@
 const exec = require("child_process").exec;
 const fs = require("fs");
 const chalk = require("chalk");
-const endOfLine = require('os').EOL;
+const endOfLine = require("os").EOL;
+const depcheck = require("depcheck");
 
 const ignore = [
-	"depcheck", // Used in this script,
-	"babel-cli", // used to compile in scripts/build.js
-	"babel-plugin-dev-expression", // Used in scripts/babel-preset.js
-	"babel-plugin-transform-react-remove-prop-types", // Used in scripts/babel-preset.js
-	"babel-preset-es2015", // Used in scripts/babel-preset.js
-	"babel-preset-react", // Used in scripts/babel-preset.js
-	"babel-preset-stage-1", // Used in scripts/babel-preset.js
-	"husky", // Used in combination with lint-staged
-	"prettier", // Used by lint-staged,
-	"eslint-config-plugin:react", // Already there, but with "-" instead of ":",
-    "npm-check-updates", // used by this script,
+    "depcheck", // Used in this script,
+    "babel-cli", // used to compile in scripts/build.js
+    "husky", // Used in combination with lint-staged
+    "prettier", // Used by lint-staged,
+    "eslint-config-plugin:react", // Already there, but with "-" instead of ":"
 ];
 
 const packages = fs.readdirSync("./../").map(item => "@stejar/" + item);
 
 exec("git status -s", (err, std) => {
-    const modified = std.split(endOfLine).filter(item => !!item).filter(item => item.indexOf("../") === -1);
+    const modified = std
+        .split(endOfLine)
+        .filter(item => !!item)
+        .filter(item => item.indexOf("../") === -1);
 
     if (modified.length > 0) {
-        console.log(chalk.bgRed.bold('You must commit your files to GIT before upgrading your dependencies versions.'));
+        console.log(chalk.bgRed.bold("You must commit your files to GIT before upgrading your dependencies versions."));
         process.exit(1);
         return;
     }
 
-    exec("depcheck --ignores " + ignore.concat(packages).join(","), (err,std) => {
-        if (std.indexOf("No depcheck issue") === -1) {
-            console.log(std);
-            console.log(chalk.bgRed.bold('Issues with dependencies'));
+    const options = {
+        ignoreMatches: ignore.concat(packages),
+    };
+
+    depcheck(process.cwd(), options, result => {
+        let error = false;
+        if (Object.keys(result.missing).length > 0) {
+            error = true;
+            console.log('');
+            console.log(chalk.bgRed.bold("Missing dependencies:"));
+            Object.keys(result.missing).map(item => console.log(item + " used in " + result.missing[item]));
+        }
+
+        if (Object.keys(result.devDependencies).length > 0) {
+            console.log('');
+            console.log(chalk.bgYellow.bold("Missing DEV dependencies (but not considered an issue):"));
+            result.devDependencies.map(item => console.log(item));
+        }
+
+        if (error) {
+            console.log(chalk.bgRed.bold("Issues with dependencies !"));
             process.exit(1);
-            return;
+            return
         }
     });
 });
-
