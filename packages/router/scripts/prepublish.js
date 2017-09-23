27bbf244f94a121531fd6e104fb4c730e0c05875
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2017-09-23 13:13:07 +0300

    wip

--- packages/router/scripts/prepublish.js
+++
@@ -1,6 +0,0 @@
-const exec = require("child_process").execSync;
-
-const result = exec("eslint", {encoding: "utf-8"});
-console.log(result);
-const result = exec("npm run build", {encoding: "utf-8"});
-console.log(result);
