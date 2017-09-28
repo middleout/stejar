2545a2213e92a4050d1fbbb993a862c1b786747a
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2017-09-28 08:40:13 +0300

    wip

---
+++ scripts/onPrepublish.js
@@ -0,0 +1,16 @@
+const execSync = require("child_process").execSync;
+
+const exec = (command) =>
+    execSync(command, {
+        stdio: "inherit",
+        env: Object.assign({}, process.env),
+    });
+
+module.exports = () => {
+    console.log('Prepublishing ...');
+    exec("npm run test:run");
+    exec("npm run build");
+    // TODO:
+    // check the "files" property inside the package.json to make sure it has all the files from the "build" - basically all the .js files
+}
+
