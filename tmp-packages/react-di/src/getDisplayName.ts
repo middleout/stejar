686f84321ba491eee7b89f17fbd1345e0f2b9fdf
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2017-09-28 11:02:40 +0300

    react-di done

--- tmp-packages/react-di/src/getDisplayName.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @param WrappedComponent
- * @returns {string|string}
- */
-export function getDisplayName(WrappedComponent: any): string {
-    return WrappedComponent.displayName || WrappedComponent.name || "AnonymousComponent";
-}
--- tmp-packages/react-di/typings/inject.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A public Higher Order Component to inject specific Service Manager
- * dependencies inside a component via props
- *
- * @param mapItemsToProps
- * @returns {(WrappedComponent:any)=>any}
- */
-export declare function inject<P>(mapItemsToProps: P): any;
