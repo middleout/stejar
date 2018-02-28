aba0bcf9e5b2f68a3071a1d8f3224381bd290239
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2018-02-28 20:09:03 +0200

    wip

---
+++ src/react-di/src/ServiceProvider.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import { Children, Component } from "react";
+// import React, { Children, Component, createContext } from "react";
+
+export class ServiceProvider extends Component {
+    /**
+     * @type {{serviceManager: Validator<T>, children: Validator<any>}}
+     */
+    static propTypes = {
+        children: PropTypes.element.isRequired,
+        serviceManager: PropTypes.object.isRequired,
+    };
+
+    /**
+     * @type {ServiceManager}
+     */
+    _serviceManager = null;
+
+    /**
+     * @param props
+     * @param context
+     */
+    constructor(props) {
+        super(props);
+        this._serviceManager = props.serviceManager;
+    }
+
+    render() {
+        return Children.only(this.props.children);
+    }
+}
