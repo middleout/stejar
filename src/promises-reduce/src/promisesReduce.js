941b5626b298d4790c1c9f8021defa9715b85bcb
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2018-03-25 18:53:36 +0300

    remove promises-reduce in favor of "items-promise"

--- src/promises-reduce/src/promisesReduce.js
+++
@@ -1,8 +0,0 @@
-export function promisesReduce(promisesFactories) {
-    return (...args) => {
-        promisesFactories.reduce(
-            (promise, func) => promise.then(result => func(...args).then(Array.prototype.concat.bind(result))),
-            Promise.resolve([])
-        );
-    };
-}
