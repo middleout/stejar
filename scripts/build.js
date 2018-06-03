6f79e887dd400050751d72f8b860da74285e253c
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2018-06-03 10:25:14 +0300

    added support for D.TS copy froms src to "es" for automcplete

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
-const { lstatSync, readdirSync } = require("fs");
+const { lstatSync, readdirSync, statSync, copySync } = require("fs-extra");
+const nanomatch = require("nanomatch");
 const { join } = require("path");
 const { execSync } = require("child_process");
 const args = process.argv;
@@ -12,6 +13,22 @@ const exec = (command, extraEnv) =>
 
 const isDirectory = (source, name) => lstatSync(join(source, name)).isDirectory();
 const getDirectories = source => readdirSync(source).filter(name => isDirectory(source, name));
+var getFiles = function(dir) {
+    var results = [];
+    var list = readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + "/" + file;
+        var stat = statSync(file);
+        if (stat && stat.isDirectory()) {
+            /* Recurse into a subdirectory */
+            results = results.concat(getFiles(file));
+        } else {
+            /* Is a file */
+            results.push(file);
+        }
+    });
+    return results;
+};
 
 const dirs = getDirectories("./src")
     .filter(item => !target || item == target)
@@ -21,6 +38,11 @@ const dirs = getDirectories("./src")
 dirs.forEach(dir => {
     console.log(`Building ${dir} \n--`);
     exec(`babel src/${dir}/src -d src/${dir}/es --ignore tests`);
+
+    nanomatch(getFiles(`src/${dir}/src`), "**/*.d.ts").forEach(path => {
+        copySync(path, path.replace(`src/${dir}/src`, `src/${dir}/es`));
+    });
+
     console.info(`Built ${dir} !\n--`);
     console.log(`\n`);
 });
