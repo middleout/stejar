15f15e87a463172dc5fa2c105845df695e8c2d7b
Andrei Gabreanu <andrei.gabreanu@middleout.com>
2019-06-05 15:20:49 +0300

    better like this

--- src/router-middleware/src/makeAsyncMiddleware.js
+++ src/router-middleware/src/makeAsyncMiddleware.js
@@ -1,16 +1,16 @@
 export function makeAsyncMiddlewares(map) {
     const middlewares = {};
     Object.keys(map).forEach(key => {
-        middlewares[key] = makeMiddleware(map[key], key);
+        middlewares[key] = makeMiddleware(map[key]);
     });
 
     return middlewares;
 }
 
-function makeMiddleware(importer, name = "default") {
+function makeMiddleware(importer) {
     return function middleware(routeMatch, container) {
         return importer()
-            .then(module => module[name](routeMatch, container))
+            .then(middleware => middleware(routeMatch, container))
             .then(component => {
                 const singleRoute = routeMatch.routes.find(item => item.middleware === middleware);
                 if (!singleRoute.component && component) {
